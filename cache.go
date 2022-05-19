f71e5e19db99d72f22e204856e106a49aad7e36a
Viachaslau_Zhuk <Zhukviachaslau@gmail.com>
2022-05-19 22:45:51 +0300

    init commit

---
+++ cache.go
@@ -0,0 +1,35 @@
+package cache
+
+// type Point struct {
+// 	x, y int
+// }
+
+// type Square struct {
+// 	start Point
+// 	a     uint
+// }
+
+// func (r *Square) Area() uint {
+// 	return r.a * r.a
+// }
+
+// func (r Square) Perimeter() uint {
+// 	return 4 * r.a
+// }
+
+// func (r Square) End() Point {
+// 	out := Point{r.start.x + int(r.a), r.start.y + int(r.a)}
+// 	return out
+// }
+
+// package main
+
+// func main() {
+// 	//	m := "+1-2 -3 -5"
+// 	m := "-a-2"
+// 	var a string
+// 	fmt.Println(m)
+// 	a, e := StringSum(m)
+// 	fmt.Println("string", a)
+// 	fmt.Println("error: ", e)
+// }
